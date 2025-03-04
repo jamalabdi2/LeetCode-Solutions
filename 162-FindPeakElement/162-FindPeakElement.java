d97501d91c341c709a57a25e3cbcd82f778e1b66
Jumale Abdi Jillo <113813239+jamalabdi2@users.noreply.github.com>
2025-03-04 22:32:30 +0900

    File updated at 04/03/2025, 22:32:30

--- 162-FindPeakElement/162-FindPeakElement.java
+++ 162-FindPeakElement/162-FindPeakElement.java
@@ -1,17 +1,18 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-        if(nums.length == 1) return 0;
-        for(int i = 0; i < nums.length; i++){
-            if( i > 0 && i < nums.length - 1){
-                if(nums[i] > nums[i - 1] && nums[i] > nums[i+1]){
-                    return i;
-                }
-            }else if(i == 0){
-                if(nums[i] > nums[i + 1]) return i;
-            }else{
-                if(nums[i] > nums[i - 1]) return i;
-            }
-        }
-        return 0;
+        int n = nums.length;
+       //check the case when we have one element
+       if(n == 1) return 0; // first element is the greatest
+       //check firt element and second elements
+       if(nums[0] > nums[1]) return 0;
+
+       //check for the rest
+       for(int i = 1; i < n - 1; i++ ){
+            if(nums[i] > nums[i - 1] && nums[i] > nums[i+1]) return i;
+       }
+
+       //check for the last element
+       if(nums[n - 1] > nums[n - 2]) return n - 1;
+       return -1;
     }
 }
