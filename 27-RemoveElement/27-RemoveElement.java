a1fc4f20f9569c271147d01b6f9e0fae9e9fc1f8
Jumale Abdi Jillo <113813239+jamalabdi2@users.noreply.github.com>
2025-03-05 01:18:08 +0900

    File updated at 05/03/2025, 01:18:07

--- 27-RemoveElement/27-RemoveElement.java
+++ 27-RemoveElement/27-RemoveElement.java
@@ -2,7 +2,7 @@ class Solution {
     public int removeElement(int[] nums, int val) {
         int skip = 0;
         for(int i = 0; i < nums.length; i++){
-            if(nums[i] != val && skip >= 0){
+            if(nums[i] != val){
                 nums[i - skip] = nums[i];
             }else{
                 skip++;
