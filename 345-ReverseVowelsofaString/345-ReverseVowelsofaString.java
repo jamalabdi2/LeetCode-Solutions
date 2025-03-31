7456fb69de9d16258054ba62279335490568fb5c
Jumale Abdi Jillo <113813239+jamalabdi2@users.noreply.github.com>
2025-03-31 10:37:53 +0900

    File updated at 31/03/2025, 10:37:53

--- 345-ReverseVowelsofaString/345-ReverseVowelsofaString.java
+++ 345-ReverseVowelsofaString/345-ReverseVowelsofaString.java
@@ -1,32 +1,34 @@
-// Last updated: 31/03/2025, 10:32:18
+// Last updated: 31/03/2025, 10:37:53
 class Solution {
+    private static final HashSet<Character> VOWELS = new HashSet<>();
+    static{
+        for(char c: "AEIOUaeiou".toCharArray()) VOWELS.add(c);
+    }
     public String reverseVowels(String s) {
         int left = 0;
         int right = s.length() - 1;
         char[] charArray = s.toCharArray();
-
-        while (left < right) { 
-            char leftChar = charArray[left];
-            char rightChar = charArray[right];
-
-            if (isValid(leftChar) && isValid(rightChar)) { 
-                swap(charArray, left, right);
+        
+        while(left < right){
+            //move left pointer forward until we find a vowel
+            while(left < right && !isValid(charArray[left])){
                 left++;
+            } 
+            while(left < right && !isValid(charArray[right])){
                 right--;
-            } else if (isValid(leftChar)) {
-                right--;
-            } else {
-                left++;
-            }
+            }   
+            swap(charArray,left,right);
+            left++;
+            right--;
+
         }
         return new String(charArray);
+        
     }
-
-    public boolean isValid(char c) {
-        return "aeiouAEIOU".indexOf(c) != -1;
+    public boolean isValid(char c){
+        return VOWELS.contains(c);
     }
-
-    public void swap(char[] arr, int left, int right) {
+    public void swap(char[] arr, int left,int right){
         char temp = arr[left];
         arr[left] = arr[right];
         arr[right] = temp;
