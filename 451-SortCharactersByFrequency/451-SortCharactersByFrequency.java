397260f6cc41cc42eb987c4b7c5e326d75d21cf2
Jumale Abdi Jillo <113813239+jamalabdi2@users.noreply.github.com>
2025-04-22 15:42:10 +0900

    File updated at 22/04/2025, 15:42:09

--- 451-SortCharactersByFrequency/451-SortCharactersByFrequency.java
+++ 451-SortCharactersByFrequency/451-SortCharactersByFrequency.java
@@ -1,34 +1,17 @@
-// Last updated: 22/04/2025, 15:10:37
+// Last updated: 22/04/2025, 15:42:09
 class Solution {
-    public String frequencySort(String s) {
-        String ans = sortString(s);
-        return ans;
-    }
-    public static String sortString(String s){
-        int[] freq = new int[256];
-        for(char c: s.toCharArray()) freq[c]++;
-
-        StringBuilder[] buckets = new StringBuilder[s.length() + 1];
-        for(int i = 0; i < 256; i++){
-            int f = freq[i];
-            if(f > 0){
-                if(buckets[f] == null){
-                    buckets[f] = new StringBuilder();
-                }
-                buckets[f].append( (char) i);
+    public int firstUniqChar(String s) {
+        int[] freq = new int[26];
+        for(char c: s.toCharArray()){
+            freq[c - 'a']++;
+        }
+        for(int i = 0; i < s.length(); i++){
+            int index = s.charAt(i) - 'a';
+            if(freq[index] == 1){
+                return i;
             }
         }
-
-        //we have characters in the buckets
-        //so we have to fill 
-        StringBuilder sb = new StringBuilder();
-        for(int i = buckets.length - 1; i >= 0; i--){
-            if(buckets[i] != null){
-                for(char c: buckets[i].toString().toCharArray()){
-                    sb.append(String.valueOf(c).repeat(i));
-                }
-            }
-        }
-        return sb.toString();
+        return -1;
+        
     }
 }
