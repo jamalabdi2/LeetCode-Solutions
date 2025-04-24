5c25e1bf1150a65ce8657303a9fcc7ded3553175
Jumale Abdi Jillo <113813239+jamalabdi2@users.noreply.github.com>
2025-04-25 01:00:40 +0900

    File updated at 25/04/2025, 01:00:40

--- 387-FirstUniqueCharacterinaString/387-FirstUniqueCharacterinaString.java
+++ 387-FirstUniqueCharacterinaString/387-FirstUniqueCharacterinaString.java
@@ -1,23 +1,25 @@
-// Last updated: 25/04/2025, 00:57:54
+// Last updated: 25/04/2025, 01:00:40
 class Solution {
     public int mostFrequentEven(int[] nums) {
         int[] count = new int[100001];
-        for(int num: nums){
-            if(num % 2 == 0){
+        
+        for (int num : nums) {
+            if (num % 2 == 0) {
                 count[num]++;
             }
         }
 
-        int maxf = Arrays.stream(count).max().getAsInt();
-        for(int i = 0; i < count.length; i++){
-            if(count[i] > 0 && count[i] == maxf){
-                return i;
+        int maxFreq = 0;
+        int result = -1;
+
+    
+        for (int i = 0; i < count.length; i++) {
+            if (count[i] > maxFreq) {
+                maxFreq = count[i];
+                result = i;
             }
         }
-        if (maxf > 0){
-            return count[maxf];
-        }else{
-            return -1;
-        }
+
+        return result;
     }
 }
