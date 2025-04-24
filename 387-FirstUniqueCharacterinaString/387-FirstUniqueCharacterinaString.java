03cbce8c11cb4aaa0aa8f3110f16eb3e35fa6c57
Jumale Abdi Jillo <113813239+jamalabdi2@users.noreply.github.com>
2025-04-25 01:11:10 +0900

    File updated at 25/04/2025, 01:11:09

--- 387-FirstUniqueCharacterinaString/387-FirstUniqueCharacterinaString.java
+++ 387-FirstUniqueCharacterinaString/387-FirstUniqueCharacterinaString.java
@@ -1,25 +1,23 @@
-// Last updated: 25/04/2025, 01:07:11
+// Last updated: 25/04/2025, 01:11:09
 class Solution {
     public int mostFrequentEven(int[] nums) {
-        int[] count = new int[100001];
-        
-        for (int num : nums) {
-            if (num % 2 == 0) {
-                count[num]++;
+       HashMap<Integer,Integer> map = new HashMap<>();
+
+       for(int num: nums){
+            if(num % 2 == 0){
+                map.put(num, map.getOrDefault(num, 0) + 1);
             }
-        }
-
-        int maxFreq = 0;
-        int result = -1;
-
-    
-        for (int i = 0; i < count.length; i++) {
-            if (count[i] > maxFreq) {
-                maxFreq = count[i];
-                result = i;
+       }
+       int maxF = 0;
+       int result = -1;
+       for(Map.Entry<Integer,Integer> entry: map.entrySet()){
+            int value = entry.getValue();
+            int key = entry.getKey();
+            if( value > maxF || (value == maxF && key < result)){
+                maxF = value;
+                result = key;
             }
-        }
-
-        return result;
+       }
+       return result;
     }
 }
