332d7a6bd0b6fed375c5a52bedfad9d3c610cf00
Jumale Abdi Jillo <113813239+jamalabdi2@users.noreply.github.com>
2025-04-24 23:45:52 +0900

    File updated at 24/04/2025, 23:45:52

--- 2278-PercentageofLetterinString/2278-PercentageofLetterinString.java
+++ 2278-PercentageofLetterinString/2278-PercentageofLetterinString.java
@@ -1,13 +1,10 @@
-// Last updated: 24/04/2025, 23:42:17
+// Last updated: 24/04/2025, 23:45:52
 class Solution {
     public int percentageLetter(String s, char letter) {
-        int[] count = new int[26];
-        for(int i = 0; i < s.length(); i++){
-            char c = s.charAt(i);
-            count[c - 'a']++;
-        }
-    
-        int letterCount = count[letter - 'a'];
-        return (letterCount * 100) / s.length();
+        int count = 0;
+       for(int i = 0; i < s.length(); i++){
+            if(letter == s.charAt(i)) count++;
+       }
+       return (count * 100) / s.length();
     }
 }
