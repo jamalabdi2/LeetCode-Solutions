f1af02c9e796d2524f9ce8e51c62b22936a4edf0
Jumale Abdi Jillo <113813239+jamalabdi2@users.noreply.github.com>
2025-04-28 12:19:58 +0900

    File updated at 28/04/2025, 12:19:58

--- 2270-NumberofWaystoSplitArray/2270-NumberofWaystoSplitArray.java
+++ 2270-NumberofWaystoSplitArray/2270-NumberofWaystoSplitArray.java
@@ -1,10 +1,10 @@
-// Last updated: 28/04/2025, 12:18:11
+// Last updated: 28/04/2025, 12:19:58
 class Solution {
     public int waysToSplitArray(int[] nums) {
         int n = nums.length;
        long[] prefix = new long[n];
        prefix[0] = nums[0];
-       for(int i = 1; i < n; i++) prefix[i] += prefix[i - 1] + nums[i];
+       for(int i = 1; i < n; i++) prefix[i] = prefix[i - 1] + nums[i];
        
        int count = 0;
        for(int i = 0; i < n - 1; i++){
